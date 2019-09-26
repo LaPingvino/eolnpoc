0eb1ff52c78301f61fb9161211d311f61fd65f2f
Joop Kiefte <ikojba@gmail.com>
2019-09-26 04:08:33 +0200

    Add first reference to main.go

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/lapingvino/eolnpoc/olnjson"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Printf("%#v\n", olnjson.Format{})
 }
